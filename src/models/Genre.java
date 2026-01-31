5d211b69173afc5eea86846f037a612fb029c470
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2026-01-31 10:49:49 +0000

    a

--- src/models/Genre.java
+++ src/models/Genre.java
@@ -3,17 +3,23 @@ package models;
 import java.awt.Checkbox;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.Function;
 
 public record Genre(int id, String name) {
     private static final Function<List<Checkbox>, List<String>> filterSelectedGenres = (checkboxes) ->
             checkboxes.stream().filter(Checkbox::getState).map(Checkbox::getLabel).toList();
 
-    public static BiFunction<List<Checkbox>, List<Genre>, List<Genre>> getSelectedGenres = (checkboxes, genres) ->
-            genres.stream().filter(genre -> filterSelectedGenres.apply(checkboxes).stream()
-                            .anyMatch(label -> label.equals(genre.name())))
-                    .toList();
-    public static Function<List<Checkbox>, Boolean> hasSelectedGenre = (checkboxes) -> checkboxes.stream().anyMatch(Checkbox::getState);
+    private static final BiPredicate<String, Genre> filterLabel = (label, genre) -> label.equals(genre.name());
+    public static BiFunction<List<Checkbox>, List<Genre>, List<Genre>> getSelectedGenres =
+            (checkboxes, genres) ->
+                    genres.stream()
+                            .filter(genre -> filterSelectedGenres.apply(checkboxes).stream()
+                                    .anyMatch(label -> filterLabel.test(label, genre)))
+                            .toList();
+    public static Function<List<Checkbox>, Boolean> hasSelectedGenre = (checkboxes) ->
+            checkboxes.stream()
+                    .anyMatch(Checkbox::getState);
 
     public static Function<List<Genre>, List<Checkbox>> createGenreCheckboxes = (genres) ->
             genres.stream()
