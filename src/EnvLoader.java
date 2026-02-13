6a2d6706c137d53df58ab230b9c94dc6ab61ca7c
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2026-02-13 13:57:35 +0000

    a

--- src/EnvLoader.java
+++ src/EnvLoader.java
@@ -1,13 +1,12 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 public class EnvLoader {
     private EnvLoader() {
     }
-    public static Properties load(){
+
+    public static Properties load() {
         Properties properties = new Properties();
         try (InputStream input = EnvLoader.class.getClassLoader().getResourceAsStream("config.env")) {
             properties.load(input);
@@ -18,23 +17,6 @@ public class EnvLoader {
         return properties;
 
     }
-
-
-
-
-
-    public static Properties load22(){
-        var props = new Properties();
-        var envFile = Paths.get("src/config.env");
-        try (var inputStream = Files.newInputStream(envFile)) {
-            props.load(inputStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return props;
-    }
-
-
 }
 
 
